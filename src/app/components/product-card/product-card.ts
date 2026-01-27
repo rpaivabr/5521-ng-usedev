922422c9d57cec46ec53641b040f32e6139e0c3b
Rafael Paiva <r.paivabr@gmail.com>
2026-01-27 14:59:23 -0300

    video 2.1

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../product';
 
 @Component({
   selector: 'app-product-card',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.css',
 })
 export class ProductCard {
-
+  product = input.required<Product>()
 }
