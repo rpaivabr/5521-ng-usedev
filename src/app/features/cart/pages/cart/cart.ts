15ec6fdac7d28a68b3344b622c3f2a1b0c2a17f1
Rafael Paiva <r.paivabr@gmail.com>
2026-02-16 10:18:12 -0300

    video 5.4

--- src/app/features/cart/pages/cart/cart.ts
+++ src/app/features/cart/pages/cart/cart.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { CartData } from '../../shared/services/cart-data';
+import { CartData } from '../../../../shared/services/cart-data';
+import { CartItemRow } from '../../components/cart-item-row/cart-item-row';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartItemRow],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
